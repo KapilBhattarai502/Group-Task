247e0300339992a8b5a9882c78a1a5c7f38ad58b
Kapil Bhattarai <kapilbhattarai502@gmail.com>
2024-11-18 10:58:44 +0545

    feat:added footer header and homepage

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
+
+const Homepage = () => {
+  return (
+    <div>
+
+        <Header/>
+        <div>
+            content
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Homepage
